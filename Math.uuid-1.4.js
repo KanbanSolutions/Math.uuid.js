fb9597ce80387fd905f341e7e6484ef84f7f7c53
Phil Gold <pgold@rubixmedia.com>
2013-12-16 14:52:27 -0500

    replacing jquery package with original broofa source

--- Math.uuid-1.4.js
+++ Math.uuid-1.4.js
@@ -1,86 +1,92 @@
-/*
-File: Math.uuid.js
-Version: 1.4
-Change History:
-  v1.0 - first release
-  v1.1 - less code and 2x performance boost (by minimizing calls to Math.random())
-  v1.2 - Add support for generating non-standard uuids of arbitrary length
-  v1.3 - Fixed IE7 bug (can't use []'s to access string chars.  Thanks, Brian R.)
-  v1.4 - Changed method to be "Math.uuid". Added support for radix argument.  Use module pattern for better encapsulation.
+/*!
+Math.uuid.js (v1.4)
+http://www.broofa.com
+mailto:robert@broofa.com
 
-Latest version:   http://www.broofa.com/Tools/Math.uuid.js
-Information:      http://www.broofa.com/blog/?p=151
-Contact:          robert@broofa.com
-----
-Copyright (c) 2008, Robert Kieffer
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-
-    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-    * Neither the name of Robert Kieffer nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+Copyright (c) 2010 Robert Kieffer
+Dual licensed under the MIT and GPL licenses.
 */
 
-jQuery.pkg('Math.uuid-1.4.pkg.js', {
-  init: function(){
-    /*
-     * Generate a random uuid.
-     *
-     * USAGE: Math.uuid(length, radix)
-     *   length - the desired number of characters
-     *   radix  - the number of allowable values for each character.
-     *
-     * EXAMPLES:
-     *   // No arguments  - returns RFC4122, version 4 ID
-     *   >>> Math.uuid()
-     *   "92329D39-6F5C-4520-ABFC-AAB64544E172"
-     * 
-     *   // One argument - returns ID of the specified length
-     *   >>> Math.uuid(15)     // 15 character ID (default base=62)
-     *   "VcydxgltxrVZSTV"
-     *
-     *   // Two arguments - returns ID of the specified length, and radix. (Radix must be <= 62)
-     *   >>> Math.uuid(8, 2)  // 8 character ID (base=2)
-     *   "01001010"
-     *   >>> Math.uuid(8, 10) // 8 character ID (base=10)
-     *   "47473046"
-     *   >>> Math.uuid(8, 16) // 8 character ID (base=16)
-     *   "098F4D35"
-     */
-    Math.uuid = (function() {
-      // Private array of chars to use
-      var CHARS = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split(''); 
-    
-      return function (len, radix) {
-        var chars = CHARS, uuid = [], rnd = Math.random;
-        radix = radix || chars.length;
-    
-        if (len) {
-          // Compact form
-          for (var i = 0; i < len; i++) uuid[i] = chars[0 | rnd()*radix];
-        } else {
-          // rfc4122, version 4 form
-          var r;
-    
-          // rfc4122 requires these characters
-          uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-';
-          uuid[14] = '4';
-    
-          // Fill in random data.  At i==19 set the high bits of clock sequence as
-          // per rfc4122, sec. 4.1.5
-          for (var i = 0; i < 36; i++) {
-            if (!uuid[i]) {
-              r = 0 | rnd()*16;
-              uuid[i] = chars[(i == 19) ? (r & 0x3) | 0x8 : r & 0xf];
-            }
-          }
+/*
+ * Generate a random uuid.
+ *
+ * USAGE: Math.uuid(length, radix)
+ *   length - the desired number of characters
+ *   radix  - the number of allowable values for each character.
+ *
+ * EXAMPLES:
+ *   // No arguments  - returns RFC4122, version 4 ID
+ *   >>> Math.uuid()
+ *   "92329D39-6F5C-4520-ABFC-AAB64544E172"
+ *
+ *   // One argument - returns ID of the specified length
+ *   >>> Math.uuid(15)     // 15 character ID (default base=62)
+ *   "VcydxgltxrVZSTV"
+ *
+ *   // Two arguments - returns ID of the specified length, and radix. (Radix must be <= 62)
+ *   >>> Math.uuid(8, 2)  // 8 character ID (base=2)
+ *   "01001010"
+ *   >>> Math.uuid(8, 10) // 8 character ID (base=10)
+ *   "47473046"
+ *   >>> Math.uuid(8, 16) // 8 character ID (base=16)
+ *   "098F4D35"
+ */
+(function() {
+  // Private array of chars to use
+  var CHARS = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
+
+  Math.uuid = function (len, radix) {
+    var chars = CHARS, uuid = [], i;
+    radix = radix || chars.length;
+
+    if (len) {
+      // Compact form
+      for (i = 0; i < len; i++) uuid[i] = chars[0 | Math.random()*radix];
+    } else {
+      // rfc4122, version 4 form
+      var r;
+
+      // rfc4122 requires these characters
+      uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-';
+      uuid[14] = '4';
+
+      // Fill in random data.  At i==19 set the high bits of clock sequence as
+      // per rfc4122, sec. 4.1.5
+      for (i = 0; i < 36; i++) {
+        if (!uuid[i]) {
+          r = 0 | Math.random()*16;
+          uuid[i] = chars[(i == 19) ? (r & 0x3) | 0x8 : r];
         }
-    
-        return uuid.join('');
-      };
-    })();
-  }
-});
+      }
+    }
+
+    return uuid.join('');
+  };
+
+  // A more performant, but slightly bulkier, RFC4122v4 solution.  We boost performance
+  // by minimizing calls to random()
+  Math.uuidFast = function() {
+    var chars = CHARS, uuid = new Array(36), rnd=0, r;
+    for (var i = 0; i < 36; i++) {
+      if (i==8 || i==13 ||  i==18 || i==23) {
+        uuid[i] = '-';
+      } else if (i==14) {
+        uuid[i] = '4';
+      } else {
+        if (rnd <= 0x02) rnd = 0x2000000 + (Math.random()*0x1000000)|0;
+        r = rnd & 0xf;
+        rnd = rnd >> 4;
+        uuid[i] = chars[(i == 19) ? (r & 0x3) | 0x8 : r];
+      }
+    }
+    return uuid.join('');
+  };
+
+  // A more compact, but less performant, RFC4122v4 solution:
+  Math.uuidCompact = function() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+      return v.toString(16);
+    });
+  };
+})();
